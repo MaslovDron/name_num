d36fe9c5ca852426e676e6c6f8d2c0aa27b2eb26
MaslovDron1 <and1977@ya.ru>
2026-03-22 20:27:05 +0300

    Create fio.php

---
+++ fio.php
@@ -0,0 +1,59 @@
+<?php
+include '../app/include/config.php';
+include '../app/include/connect.php';
+include '../app/include/functions-adm.php';
+include '../app/controllers/NameController.php';
+// Проверяем, активна ли сессия вообще
+if (empty($_SESSION['id_num'])) {
+    header('Location: ' . ABS_PATH . 'supp/index.php');
+    exit; // Важно добавить exit после header!
+}
+// Проверяем, активна ли сессия вообще
+$id=$_SESSION['id_num'];
+// if(!isset($id)){
+//     header('location:index.php');
+//     exit();
+//  }
+ ?>
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Цифровой код имени</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+   <!-- font awesome cdn link  -->
+   <!-- style-->
+   <link rel="stylesheet" href="<?=ABS_PATH?>assets/css/adm-style.css">
+   <!-- style-->
+</head>
+<body>
+<?php
+//tt($_SESSION);
+include '../app/include/admin-header.php';
+?>
+</body>
+<section class="content">
+    <div class="add-statii">
+        <p>
+            <?php
+                echo $errMsg;
+            ?>
+        </p>
+        <form action="" method="post">
+            <label for="firstname">Имя</label>
+            <input type="text" name="FirstName" id="firstname" class="form-input">
+            <label for="lastname">Отчество</label>
+            <input type="text" name="LastName" id="lastname" class="form-input">
+            <label for="famely">Фамилия</label>
+            <input type="text" name="Famely" id="famely" class="form-input">
+            
+            <input type="submit" value="Рассчитать" name="submitNameBack" class="btn1">
+        </form>
+    </div>
+</section>
+</body>
+</html>
