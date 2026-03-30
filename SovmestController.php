bc5c980a24d96369ead3a645835b10e33e1e4880
MaslovDron1 <and1977@ya.ru>
2026-03-30 19:36:30 +0300

    Update SovmestController.php

--- SovmestController.php
+++ SovmestController.php
@@ -4,77 +4,8 @@ $name1='';
 $name2='';
 $ch1='';
 $Sovmest=selectOne('calc', ['id'=>11]);
-//на фронте
-if($_SERVER['REQUEST_METHOD']=='POST' && isset ($_POST['SovmFr']))
-{
-    //tt($_POST);
-    $name1 = htmlspecialchars(trim($_POST['name1'] ?? ''), ENT_QUOTES, 'UTF-8');
-    $name2 = htmlspecialchars(trim($_POST['name2'] ?? ''), ENT_QUOTES, 'UTF-8');
-    if (strpos($name1, ' ') !== false) 
-        {
-            $name_1 = explode(' ', $name1)[0];
-        }
-         else 
-        {
-            $name_1 = $name1;
-        }
-         if (strpos($name2, ' ') !== false) 
-        {
-            $name_2 = explode(' ', $name2)[0];
-        }
-         else 
-        {
-            $name_2 = $name2;
-        }
-    if(empty($name_1)) 
-        {
-            $errMsg .= 'Пожалуйста, введит первое имя<br>';
-        }
-     if(empty($name_2)) 
-        {
-            $errMsg .= 'Пожалуйста, введит второе имя<br>';
-        }
-    if ((mb_strlen($name_1) > 20) or (mb_strlen($name_2) > 20)) 
-        {
-            $errMsg .= 'Имена не должны быть длиннее 20 символов<br>';
-        }
-    if((!preg_match('/^[а-яёА-ЯЁ]+$/u', $name_1)) or (!preg_match('/^[а-яёА-ЯЁ]+$/u', $name_2))) 
-        {
-             $errMsg .= 'Имя может содержать только русские буквы<br>';
-        }
-    if ((mb_strlen($name_1) < 2) or (mb_strlen($name_2) < 2)) 
-        {
-            $errMsg .= 'Имя должно содержать минимум 2 буквы<br>';
-        }
-            $chdate=$_POST['consent'] ?? '';
-    if($chdate=='on')
-        {
-            $ch1='checked';
-        }
-    else
-        {
-            $ch1=='';
-        }
-    if (empty($chdate)) 
-        {
-            $errMsg .='Дайте согласие на обработкуперсональных данных<br>';
-        }
-    /////////////////////////////////////////////////////////////////
-    if(empty($errMsg))//если передали все данные
-        {
-             $letterValues = [
-        'а' => 1, 'б' => 2, 'в' => 3, 'г' => 4, 'д' => 5, 'е' => 6, 'ё' => 7,
-        'ж' => 8, 'з' => 9, 'и' => 1, 'й' => 1, 'к' => 2, 'л' => 3, 'м' => 4,
-        'н' => 5, 'о' => 6, 'п' => 7, 'р' => 8, 'с' => 9, 'т' => 2, 'у' => 3,
-        'ф' => 4, 'х' => 5, 'ц' => 6, 'ч' => 7, 'ш' => 8, 'щ' => 9, 'ъ' => 1,
-        'ы' => 2, 'ь' => 3, 'э' => 4, 'ю' => 5, 'я' => 6
-    ];
-    
-    $vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
-    $consonants = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'];
-    
-    // 2. Функции
-    function reduceNumber($num) {
+//функции для расчета
+function reduceNumber($num) {
         if ($num == 11 || $num == 22 || $num == 33) return $num;
         while ($num > 9) $num = array_sum(str_split((string)$num));
         return $num;
@@ -149,7 +80,67 @@ if($_SERVER['REQUEST_METHOD']=='POST' && isset ($_POST['SovmFr']))
         
         return ['percentage' => $percentage, 'level' => $level, 'details' => $details];
     }
-    
+//функции для расчета
+//на фронте
+if($_SERVER['REQUEST_METHOD']=='POST' && isset ($_POST['SovmFr']))
+{
+    //tt($_POST);
+    $name1 = htmlspecialchars(trim($_POST['name1'] ?? ''), ENT_QUOTES, 'UTF-8');
+    $name2 = htmlspecialchars(trim($_POST['name2'] ?? ''), ENT_QUOTES, 'UTF-8');
+    if (strpos($name1, ' ') !== false) 
+        {
+            $name_1 = explode(' ', $name1)[0];
+        }
+         else 
+        {
+            $name_1 = $name1;
+        }
+         if (strpos($name2, ' ') !== false) 
+        {
+            $name_2 = explode(' ', $name2)[0];
+        }
+         else 
+        {
+            $name_2 = $name2;
+        }
+    if(empty($name_1)) 
+        {
+            $errMsg .= 'Пожалуйста, введит первое имя<br>';
+        }
+     if(empty($name_2)) 
+        {
+            $errMsg .= 'Пожалуйста, введит второе имя<br>';
+        }
+    if ((mb_strlen($name_1) > 20) or (mb_strlen($name_2) > 20)) 
+        {
+            $errMsg .= 'Имена не должны быть длиннее 20 символов<br>';
+        }
+    if((!preg_match('/^[а-яёА-ЯЁ]+$/u', $name_1)) or (!preg_match('/^[а-яёА-ЯЁ]+$/u', $name_2))) 
+        {
+             $errMsg .= 'Имя может содержать только русские буквы<br>';
+        }
+    if ((mb_strlen($name_1) < 2) or (mb_strlen($name_2) < 2)) 
+        {
+            $errMsg .= 'Имя должно содержать минимум 2 буквы<br>';
+        }
+            $chdate=$_POST['consent'] ?? '';
+    if($chdate=='on')
+        {
+            $ch1='checked';
+        }
+    else
+        {
+            $ch1=='';
+        }
+    if (empty($chdate)) 
+        {
+            $errMsg .='Дайте согласие на обработкуперсональных данных<br>';
+        }
+    /////////////////////////////////////////////////////////////////
+    if(empty($errMsg))//если передали все данные
+        {
+            include 'app/include/matr-fio.php';
+                
     // 3. Расчёт чисел для обоих имён
     $numbers1 = calculateNameNumbers($name_1, $letterValues, $vowels, $consonants);
     $numbers2 = calculateNameNumbers($name_2, $letterValues, $vowels, $consonants);
