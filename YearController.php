45ef9618e1034788151cfd058318094eb96eb0e9
MaslovDron1 <and1977@ya.ru>
2026-06-07 14:56:43 +0300

    Update YearController.php

--- YearController.php
+++ YearController.php
@@ -36,150 +36,13 @@ if($_SERVER['REQUEST_METHOD']=='POST' && isset ($_POST['FrYearCalc']))
         if(empty($errMsg))
             {
                 //подключаем расшифровку
-                include '../includde/year-form.php';
+                include 'app/include/year-front.php';
                 //подключаем расшифровку
                 //функции для расчета персонального года
                 // Функция редукции чисел (с мастер-числами 11,22,33)
-function reduceNumber($num) {
-    if ($num == 11 || $num == 22 || $num == 33) return $num;
-    while ($num > 9) $num = array_sum(str_split((string)$num));
-    return $num;
-}
-
-// Расчёт персонального года
-function calculatePersonalYear($birthdate, $targetYear = null) {
-    if ($targetYear === null) {
-        $targetYear = date('Y');
-    }
-    
-    $birthTimestamp = strtotime($birthdate);
-    if ($birthTimestamp === false) {
-        return false;
-    }
-    
-    $birthDay = date('d', $birthTimestamp);
-    $birthMonth = date('m', $birthTimestamp);
-    
-    $daySum = array_sum(str_split($birthDay));
-    $monthSum = array_sum(str_split($birthMonth));
-    $yearSum = array_sum(str_split($targetYear));
-    
-    $sum = $daySum + $monthSum + $yearSum;
-    
-    return reduceNumber($sum);
-}
-
-// ============================================
-// РАСЧЁТ ВСЕХ КОНТРОЛЬНЫХ ЧИСЕЛ
-// ============================================
-function calculateAllNumbers($birthdate, $currentYear) {
-    $birthTimestamp = strtotime($birthdate);
-    $birthDay = (int)date('d', $birthTimestamp);
-    $birthMonth = (int)date('m', $birthTimestamp);
-    $birthYear = (int)date('Y', $birthTimestamp);
-    
-    // Суммы цифр
-    $daySum = array_sum(str_split($birthDay));
-    $monthSum = array_sum(str_split($birthMonth));
-    $birthYearSum = array_sum(str_split($birthYear));
-    $currentYearSum = array_sum(str_split($currentYear));
-    
-    // Редуцированные базовые числа
-    $reducedDay = reduceNumber($daySum);
-    $reducedMonth = reduceNumber($monthSum);
-    $reducedBirthYear = reduceNumber($birthYearSum);
-    $reducedCurrentYear = reduceNumber($currentYearSum);
-    
-    // 1. Персональный год (основной)
-    $personalYearRaw = $daySum + $monthSum + $currentYearSum;
-    $personalYear = reduceNumber($personalYearRaw);
-    
-    // 2. Персональный месяц (для текущего месяца)
-    $currentMonth = (int)date('m');
-    $currentMonthSum = array_sum(str_split($currentMonth));
-    $personalMonthRaw = $daySum + $monthSum + $currentMonthSum + $currentYearSum;
-    $personalMonth = reduceNumber($personalMonthRaw);
-    
-    // 3. Кармическое число года
-    $karmicYearRaw = $daySum + $monthSum + $birthYearSum;
-    $karmicYear = reduceNumber($karmicYearRaw);
-    
-    // 4. Число судьбы (жизненный путь)
-    $lifePathRaw = $daySum + $monthSum + $birthYearSum;
-    $lifePath = reduceNumber($lifePathRaw);
-    
-    // 5. Цикл года (совпадает с персональным годом)
-    $yearCycle = $personalYear;
-    
-    // 6. Энергетические пики по кварталам
-    $quarter1 = reduceNumber($daySum + $monthSum);
-    $quarter2 = reduceNumber($daySum + $reducedCurrentYear);
-    $quarter3 = reduceNumber($monthSum + $reducedCurrentYear);
-    $quarter4 = reduceNumber($daySum + $monthSum + $reducedCurrentYear);
-    
-    // 7. Число перехода (между годами)
-    $transitionNumber = reduceNumber($personalYear + $lifePath);
-    
-    // 8. Духовное число года
-    $spiritualNumber = reduceNumber($personalYear + $karmicYear);
-    
-    // 9. Число реализации (достижений)
-    $achievementNumber = reduceNumber($personalYear + $lifePath + $karmicYear);
-    
-    // 10. Тест-число (внутренние вызовы)
-    $challengeNumber = abs($reducedDay - $reducedMonth);
-    if ($challengeNumber == 0) $challengeNumber = 9;
-    $challengeNumber = reduceNumber($challengeNumber);
-    
-    // 11. Число дня рождения (простое)
-    $birthDayNumber = $reducedDay;
-    
-    // 12. Число месяца рождения
-    $birthMonthNumber = $reducedMonth;
-    
-    // 13. Зрелость (жизненный путь + персональный год)
-    $maturityNumber = reduceNumber($lifePath + $personalYear);
-    
-    // 14. Личное число года (альтернативный расчёт)
-    $personalYearAlt = reduceNumber($reducedDay + $reducedMonth + $reducedCurrentYear);
-    
-    // Собираем всё в массив
-    return [
-        'basic' => [
-            'birth_day' => $birthDay,
-            'birth_month' => $birthMonth,
-            'birth_year' => $birthYear,
-            'current_year' => $currentYear,
-            'reduced_day' => $reducedDay,
-            'reduced_month' => $reducedMonth,
-            'reduced_birth_year' => $reducedBirthYear,
-            'reduced_current_year' => $reducedCurrentYear
-        ],
-        'main' => [
-            'personal_year' => $personalYear,
-            'personal_month' => $personalMonth,
-            'life_path' => $lifePath,
-            'karmic_year' => $karmicYear
-        ],
-        'additional' => [
-            'year_cycle' => $yearCycle,
-            'transition' => $transitionNumber,
-            'spiritual' => $spiritualNumber,
-            'achievement' => $achievementNumber,
-            'challenge' => $challengeNumber,
-            'maturity' => $maturityNumber,
-            'personal_year_alt' => $personalYearAlt,
-            'birth_day_number' => $birthDayNumber,
-            'birth_month_number' => $birthMonthNumber
-        ],
-        'quarters' => [
-            'q1' => $quarter1,
-            'q2' => $quarter2,
-            'q3' => $quarter3,
-            'q4' => $quarter4
-        ]
-    ];
-}
+///////////////////////////////////////////////////////////////////////////////////////                
+include 'app/include/pers-year-function.php';
+///////////////////////////////////////////////////////////////////////////////////////
 
                 //функции для расчета персонального года
                  $currentYear = date('Y');
@@ -191,14 +54,23 @@ function calculateAllNumbers($birthdate, $currentYear) {
         $allNumbers = calculateAllNumbers($birthDate, $currentYear);
         
         // Сохраняем результат в сессию
-        $_SESSION['year_result'] = [
-            'birthdate' => $birthDate,
-            'current_year' => $currentYear,
-            'personal_year' => $personalYear,
-            'all_numbers' => $allNumbers,
-            'interpretation' => getYearFreeInterpretation($personalYear), 
-            'calculated_at' => date('d.m.Y H:i:s')
-        ];
+      $_SESSION['year_result'] = [
+    'birthdate' => $birthDate,
+    'current_year' => $currentYear,
+    'personal_year' => $personalYear,
+    'all_numbers' => $allNumbers,
+    'interpretation' => [
+        'title' => getYearTitle($personalYear),
+        'short_desc' => getYearShort($personalYear),
+        'full_desc' => getYearFull($personalYear),
+        'opportunities' => getYearOpportunities($personalYear),
+        'warning' => getYearWarning($personalYear),
+        'color' => getYearColor($personalYear),
+        'stone' => getYearStone($personalYear),
+        'number' => getYearNumber($personalYear)
+    ],
+    'calculated_at' => date('d.m.Y H:i:s')
+];
         
         // Перенаправляем на страницу с результатом
         // $resultPage = selectOne('calc', ['id' => 12]); // ID вашего калькулятора года в БД
@@ -216,4 +88,30 @@ function calculateAllNumbers($birthdate, $currentYear) {
             }
 }
 //на фронте
+//на беке
+if($_SERVER['REQUEST_METHOD']=='POST' && isset ($_POST['BackYearCalc']))
+{
+    //tt($_POST);
+    $birthDate = $_POST['daterozd'];
+       if (empty($birthDate)) {
+        $errMsg .='Вы не ввели дату рождения<br>';
+    }
+    
+    // 2. Валидация даты
+    $date = DateTime::createFromFormat('Y-m-d', $birthDate);
+    if (!$date) {
+        $errMsg.='Вы некорректно ввели дату<br>';
+    }
+    $targetYear = trim($_POST['intgod'] ?? '');
+        if (empty($targetYear)) {
+        $errMsg .='Вы не ввели интересующий год<br>';
+    }
+    $targetYearInt = (int)$targetYear;
+    $currentYearInt = (int)date('Y');
+       if ($targetYearInt < $currentYearInt) {
+            $errMsg .= "Интересующий год ($targetYearInt) не может быть меньше текущего года ($currentYearInt)<br>";
+        }
+
+}
+//на беке
 ?>
