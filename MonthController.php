d359751fba410005e045698eab1bdfab32a19615
MaslovDron1 <and1977@ya.ru>
2026-06-14 22:55:49 +0300

    Update MonthController.php

--- MonthController.php
+++ MonthController.php
@@ -43,12 +43,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['FrMonthCalc'])) {
             // Разбираем дату рождения
         $birthDateObj = new DateTime($birthDate);
         $day = (int)$birthDateObj->format('d');
-        $month = (int)$birthDateObj->format('m');
-        
+        $month = (int)$birthDateObj->format('m');  
         // Текущий месяц и год (можно будет заменить на выбранные позже)
         $currentMonth = (int)date('n');  // 1..12
         $currentYear = (int)date('Y');
+
+        $result = calculatePersonalMonth($day, $month, $currentMonth, $currentYear);
+                
+        $_SESSION['month_result'] = [
+            'birthdate' => $birthDate,
+            'current_year' => $currentYear,
+            'current_month' => $currentMonth,
+            'current_month_name' => getRussianMonthName($currentMonth),
+            'personal_year' => $result['personal_year'],
+            'personal_month' => $result['personal_month'],
+            'calculation_details' => [
+                'day_raw' => $day,
+                'month_raw' => $month,
+                'day_reduced' => $result['details']['day_reduced'],
+                'month_reduced' => $result['details']['month_reduced'],
+                'year_reduced' => $result['details']['year_reduced'],
+                'personal_year_raw' => $result['details']['personal_year_raw'],
+                'personal_month_raw' => $result['details']['personal_month_raw']
+            ]
+        ];
         
+        header('Location: ' . ABS_PATH . 'personal-month');
+        exit;
             }
 }
 //на фронте
